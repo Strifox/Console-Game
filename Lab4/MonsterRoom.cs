997dec3bb06585e0ed9a20987088c2a2c9412633
Strifox <Johan.svensson93@outlook.com>
2017-10-04 14:15:50 +0200

    Added Combat System fields in Monster Room, also added properties (getters/setters) to private field in Monster Room.

--- Lab4/MonsterRoom.cs
+++ Lab4/MonsterRoom.cs
@@ -8,10 +8,20 @@ namespace Lab_4
 {
     public class MonsterRoom : Room
     {
+        // Combat system ints
+        private int enemyHealthPoints = 0;
+        private int enemyAttack = 0;
+        private int scoreGained = 0;
+        public string name;
+        public bool isAlive = true;
+        public int ScoreGained { get { return scoreGained; } set { scoreGained = value; } }
+        public int EnemyHealthPoints { get { return enemyHealthPoints; } set { enemyHealthPoints = value; } }
+        public int EnemyAttack { get { return enemyAttack; } set { enemyAttack = value; } }
+        public string Name { get { return name; } set { name = value; } }
+
         private string icon = "M";
-        private ConsoleColor color = ConsoleColor.Red;
+
         public override string Icon { get { return icon; } set { value = icon; } }
-        public override ConsoleColor Color { get { return color; } set { color = value; } }
         public bool hasMonster = true;
 
         public MonsterRoom() : base()
